7fd7481d98a5c38d0aebf30a172a965303464f54
Ben1897 <pjiang6@illinois.edu>
2014-10-30 22:19:33 -0500

    Change the reference unit ontology
    Change the unit ontology from the revised 'SWEET' to 'Green_Ampt3.owl':
    (1) exclude the symbols in 'Symbol' class when querying symbols in
    units;
    (2) update the semantic definitions in new ontology.

--- suc.py
+++ suc.py
@@ -8,32 +8,37 @@ def semanticUnitConversion(unit1ele, unit2ele):
     '''
     # ... load SWEET unit ontology
     g = rdflib.Graph()
-    #g.parse(r'D:\researchInUIUC\project\Ontologies\Green_Ampt3.owl')
-    g.parse(r'C:\Users\Ben1897\Desktop\SWEET_rdf_edit.owl')
+    g.parse(r'D:\researchInUIUC\project\Ontologies\Green_Ampt3.owl')
     # ... find all units with symbols for mapping
     symbol = g.query(
     """SELECT DISTINCT ?a ?b
        WHERE {
-          ?a screla:hasSymbol ?b .
+          ?a relaSci:hasSymbol ?b .
           ?a rdf:type ?c .
           FILTER ( (?c != <http://sweet.jpl.nasa.gov/2.3/reprSciUnits.owl#Prefix>) &&
-                   (?c != <http://www.w3.org/2002/07/owl#NamedIndividual>))
+                   (?c != <http://www.w3.org/2002/07/owl#NamedIndividual>) &&
+                   (?c != <http://www.semanticweb.org/ben1897/ontologies/2014/9/untitled-ontology-26#Symbol>) &&
+                   (?c != <http://www.semanticweb.org/ben1897/ontologies/2014/9/untitled-ontology-26#Variable>) &&
+                   (?c != <http://www.semanticweb.org/ben1897/ontologies/2014/9/untitled-ontology-26#Parameter>) &&
+                   (?c != <http://www.semanticweb.org/ben1897/ontologies/2014/9/untitled-ontology-26#UniversalConstant>))
        }""")
     for a,b in symbol:
         pass
     # ... search the names in ontology
     for uriref,sym in symbol:
-        name = str(uriref).replace('http://sweet.jpl.nasa.gov/2.3/reprSciUnits.owl#','')
-        if (str(name) == unit1ele or str(sym) == unit1ele):
+        #name = str(uriref).replace('http://sweet.jpl.nasa.gov/2.3/reprSciUnits.owl#','')
+        #if (str(name) == unit1ele or str(sym) == unit1ele):
+        if str(sym) == unit1ele:
             #k = raw_input('Is '+ unit1ele + ' ' + name + ' your unit (Y/N)?')
             #if k == 'Y':
-            #print name
+            #print str(uriref)
             unit1eleurl = uriref
             break
     #symbol = g.subject_objects(target)
     for uriref,sym in symbol:
-        name = str(uriref).replace('http://sweet.jpl.nasa.gov/2.3/reprSciUnits.owl#','')
-        if (str(name) == unit2ele or str(sym) == unit2ele):
+        #name = str(uriref).replace('http://sweet.jpl.nasa.gov/2.3/reprSciUnits.owl#','')
+        #if (str(name) == unit2ele or str(sym) == unit2ele):
+        if str(sym) == unit2ele:
             #k = raw_input('Is ' + unit2ele + ' '+ name + ' the standard unit (Y/N)?')
             #if k == 'Y':
             #print name
@@ -45,19 +50,19 @@ def semanticUnitConversion(unit1ele, unit2ele):
     obj2 = '<'+unit2eleurl+'>'
     bu1 = 'SELECT ?k \n \
                WHERE {\n \
-               ' + obj1 + ' screla:hasBaseUnit ?k. \
+               ' + obj1 + ' relaSci:hasBaseUnit ?k. \
                \n }'
     sn1 = 'SELECT ?k \n \
                WHERE {\n \
-               ' + obj1 + ' mrela:hasScalingNumber ?k. \
+               ' + obj1 + ' relaMath:hasScalingNumber ?k. \
                \n }'
     bu2 = 'SELECT ?k \n \
                WHERE {\n \
-               ' + obj2 + ' screla:hasBaseUnit ?k. \
+               ' + obj2 + ' relaSci:hasBaseUnit ?k. \
                \n }'
     sn2 = 'SELECT ?k \n \
                WHERE {\n \
-               ' + obj2 + ' mrela:hasScalingNumber ?k. \
+               ' + obj2 + ' relaMath:hasScalingNumber ?k. \
                \n }'
     bu1 = g.query(bu1)
     sn1 = g.query(sn1)
@@ -76,4 +81,13 @@ def semanticUnitConversion(unit1ele, unit2ele):
     return eval(c1+ '/' +c2)
 #target = rdflib.term.URIRef( \
 #u'http://sweet.jpl.nasa.gov/2.3/relaSci.owl#hasSymbol');
+#g.parse(r'C:\Users\Ben1897\Desktop\SWEET_rdf_edit.owl')
 #symbol = g.subject_objects(target)
+#    symbol = g.query(
+#    """SELECT DISTINCT ?a ?b
+#       WHERE {
+#          ?a screla:hasSymbol ?b .
+#          ?a rdf:type ?c .
+#          FILTER ( (?c != <http://sweet.jpl.nasa.gov/2.3/reprSciUnits.owl#Prefix>) &&
+#                   (?c != <http://www.w3.org/2002/07/owl#NamedIndividual>))
+#       }""")
